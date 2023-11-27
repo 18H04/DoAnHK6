7a0e294fd4af78dd652e8cc7edb69cd4f3776e67
Dang Hieu <phungdanghieu1842003@gmail.com>
2023-11-28 00:11:02 +0700

    install Module

--- Api_Server/Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
+++ Api_Server/Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c108d05da5928d75f704c3d81d65cbcf0346b87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af5ad1c09b1ebea0e7a91f4cbfcb126de573c020")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
